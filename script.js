a67087dcafd505469c04e17c9aae79311d5d39cd
00001140751189sp <00001140751189spaleducacao@gmail.com>
2024-12-02 16:52:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})<!-- código omitido -->
+
+<script src="script.js"></script>
+</body>
+</html>const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})<!-- código omitido -->
+
+<script src="script.js"></script>
+</body>
+</html>
